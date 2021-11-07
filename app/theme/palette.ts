cadf62a16a74bd6d86c29d61eb61e85c4ed89502
Sadeem Ahmad <sadeem.ahmad@pio-tech.com>
2021-11-07 13:29:01 +0200

    updated colors theme

--- app/theme/palette.ts
+++ app/theme/palette.ts
@@ -8,4 +8,22 @@ export const palette = {
   lighterGrey: "#CDD4DA",
   angry: "#dd3333",
   deepPurple: "#5D2555",
+
+  grey: {
+    type1: "#413D4D",
+    type2: "#353438",
+    type3: "#BAB6C8",
+    type4: "#707070",
+    type5: "#46454B",
+  },
+  purple: {
+    type1: "#B87BF2",
+    type2: "#938BAC",
+    type3: "#3A3048",
+    type4: "#403C4C",
+  },
+  purpleActive: { 
+    typeActive: "#d7c0db",
+    typeInactive: "#A3A2A5",
+   },
 }
